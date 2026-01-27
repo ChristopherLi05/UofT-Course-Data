7081521ecbc838dcf631b5c603547e3c22d988de
a <@>
2026-01-27 17:46:32 -0500

    add only artsci courses

--- uoft_course_list.py
+++ uoft_course_list.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pandas as pd
 
 temp = set()
 
@@ -11,7 +12,13 @@ for i in range(2023, 2026):
 temp |= {i[:-5] for i in os.listdir(f"Enrollment-Data/20229") if i.endswith(".json") and not i.lower().startswith("aa")}
 
 # sg only
-temp = [i[:-1] for i in temp if "H1" in i]
+temp = {i[:-1] for i in temp if "H1" in i}
+
+# artsci only
+df = pd.read_csv("data/course_evals.csv")
+courses = set(df["course_code"].unique())
+
+temp &= courses
 
 with open("data/courses.json", "w") as f:
-    json.dump(temp, f)
+    json.dump(list(temp), f)
